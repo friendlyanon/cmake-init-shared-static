5cdabb63ca46ab554e1fb1c133fd334eba67c1b5
friendlyanon <friendlyanon@users.noreply.github.com>
2021-04-24 22:36:58 +0200

    Update to v0.6.0

--- cmake/sharedConfig.cmake
+++ cmake/sharedConfig.cmake
@@ -1,1 +1,1 @@
-include(${CMAKE_CURRENT_LIST_DIR}/sharedTargets.cmake)
+include("${CMAKE_CURRENT_LIST_DIR}/sharedTargets.cmake")
--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -1,5 +1,9 @@
 # ---- Developer mode ----
 
+# Developer mode enables targets and code paths in the CMake scripts that are
+# only relevant for the developer(s) of shared
+# Targets necessary to build the project must be provided unconditionally, so
+# consumers can trivially build and package the project
 if(PROJECT_IS_TOP_LEVEL)
   option(shared_DEVELOPER_MODE "Enable developer mode" OFF)
   option(BUILD_SHARED_LIBS "Build shared libs." OFF)
@@ -10,14 +14,19 @@ endif()
 
 # ---- Warning guard ----
 
+# target_include_directories with the SYSTEM modifier will request the compiler
+# to omit warnings from the provided paths, if the compiler supports that
+# This is to provide a user experience similar to find_package when
+# add_subdirectory or FetchContent is used to consume this project
+set(shared_warning_guard "")
 if(NOT PROJECT_IS_TOP_LEVEL)
   option(
-      shared_INCLUDE_WITHOUT_SYSTEM
-      "Enable shared's warnings for dependents"
-      OFF
+      shared_INCLUDES_WITH_SYSTEM
+      "Use SYSTEM modifier for shared's includes, disabling warnings"
+      ON
   )
-endif()
-set(shared_warning_guard SYSTEM)
-if(PROJECT_IS_TOP_LEVEL OR shared_INCLUDE_WITHOUT_SYSTEM)
-  set(shared_warning_guard "")
+  mark_as_advanced(shared_INCLUDES_WITH_SYSTEM)
+  if(shared_INCLUDES_WITH_SYSTEM)
+    set(shared_warning_guard SYSTEM)
+  endif()
 endif()
