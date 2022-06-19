6224b23e34de84f5d698b8e6ee0edb115b30a055
friendlyanon <friendlyanon@users.noreply.github.com>
2022-06-20 01:31:32 +0200

    Update to v0.31.0

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -37,7 +37,7 @@ write_basic_package_version_file(
 
 # Allow package maintainers to freely override the path for the configs
 set(
-    shared_INSTALL_CMAKEDIR "${CMAKE_INSTALL_DATADIR}/${package}"
+    shared_INSTALL_CMAKEDIR "${CMAKE_INSTALL_LIBDIR}/cmake/${package}"
     CACHE PATH "CMake package config location relative to the install prefix"
 )
 mark_as_advanced(shared_INSTALL_CMAKEDIR)
